b5db62ce89a5ba756411d64b3a4a84d00c029223
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-08-13 18:53:20 -0400

    Insert coin

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hori_movement = Input.GetAxis("Horizontal");
+        float verti_movement = Input.GetAxis("Vertical");
+
+        Vector3 force = new Vector3(hori_movement, 0.0f, verti_movement);
+
+        rb.AddForce(force * speed);
+
+    }
+}
