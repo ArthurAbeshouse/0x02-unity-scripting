d0042469265aa995a9f4dae8d652cebae2aa4fb7
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-08-13 20:40:12 -0400

    everything else

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
 
     private Rigidbody rb;
+
+    private int score = 0;
+
+    public int health = 5;
 
     // Start is called before the first frame update
     void Start()
@@ -24,5 +29,35 @@ public class PlayerController : MonoBehaviour
 
         rb.AddForce(force * speed);
 
+    }
+
+    void FixedUpdate()
+    {
+        if (health == 0)
+        {
+            Debug.Log("Game Over!");
+            SceneManager.LoadScene("maze");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pickup"))
+        {
+            ++score;
+            Debug.Log($"Score: {score}");
+            Destroy(other.gameObject);
+        }
+        else if (other.CompareTag("Trap"))
+        {
+            --health;
+            Debug.Log($"Health: {health}");
+
+
+        }
+        else if (other.CompareTag("Goal"))
+        {
+            Debug.Log("You win!");
+        }
     }
 }
